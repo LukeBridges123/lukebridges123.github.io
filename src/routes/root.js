2fac981f2fddba1c9aad64e9b4770436eef37c7b
Luke Bridges <lukebridges123@gmail.com>
2024-11-20 20:43:59 -0500

    Fixed scrolling issue

--- src/routes/root.js
+++ src/routes/root.js
@@ -1,10 +1,23 @@
 import { Outlet, Link } from "react-router-dom";
 import '../App.css'
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+// N.B. this was copied from the React Router docs, https://v5.reactrouter.com/web/guides/scroll-restoration
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 export default function Root(){
     return (
         <>
             <NavigationBar />
             <div class ="pagebody">
+                <ScrollToTop/>
                 <Outlet />
             </div>
         </>
